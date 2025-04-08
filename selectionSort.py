c57754dade845cb58a4926add3fa5fd5f14f9377
Kunal Asude <kunalasude@gmail.com>
2025-04-08 20:16:07 +0530

    Selection Short With Python

---
+++ selectionSort.py
@@ -0,0 +1,18 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+
+print("Sorted array:", arr)
+
+# userinput = input("Enter a list of numbers separated by spaces: ")
+# userinput = userinput.split()   
+# userinput = [int(i) for i in userinput]
+# selection_sort(userinput)
+# print("Sorted user input:", userinput)
