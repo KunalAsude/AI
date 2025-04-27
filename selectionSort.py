360a0ae2d64857b3cbdb9af65083ddf6e12ac9fe
Kunal Asude <kunalasude@gmail.com>
2025-04-27 23:41:51 +0530

    Updated AI Codes

--- selectionSort.py
+++ selectionSort.py
@@ -1,18 +1,15 @@
-def selection_sort(arr):
-    for i in range(len(arr)):
-        min_index = i
-        for j in range(i + 1, len(arr)):
+def selectionSort(arr):
+    for i in range (len(arr)):
+        min_index=i
+        for j in range (i+1,len(arr)):
             if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+                min_index=j
+        arr[i],arr[min_index]=arr[min_index],arr[i]
 
-arr = [64, 25, 12, 22, 11]
-selection_sort(arr)
+        print("The output after pass "+str(i+1)+" is "+str(arr))
 
-print("Sorted array:", arr)
+inputArr= input("Enter the elemets seperated by spaces = ")
+arr=inputArr.split()
+arr=[int(i) for i in arr]
 
-# userinput = input("Enter a list of numbers separated by spaces: ")
-# userinput = user-input.split()   
-# userinput = [int(i) for i in userinput]
-# selection_sort(userinput)
-# print("Sorted user input:", userinput)
+selectionSort(arr)
